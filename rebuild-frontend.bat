88f0e74af0bf738dec869c9baef7fb94e31d3743
Majd04 <98593727+Majd04@users.noreply.github.com>
2025-11-12 09:11:46 +0100

    Docker command

---
+++ rebuild-frontend.bat
@@ -0,0 +1,11 @@
+@echo off
+ECHO 1/3: Stoppar och tar bort befintlig patient-frontend
+docker rm -f patient-frontend 2>NUL
+
+ECHO 2/3: Bygger patient-frontend
+docker build --no-cache -t patient-frontend:latest .
+
+ECHO 3/3: Startar patient-frontend
+docker run -d --name patient-frontend --network patient-network -p 80:80 patient-frontend:latest
+
+ECHO Klar
